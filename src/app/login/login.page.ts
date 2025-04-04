c36fab7f99c9d2a673b1b71b34568f95fa5941ad
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-04-04 16:08:13 -0300

    feat: inicializa service de autenticacao

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, FormsModule, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonList, IonItem, IonInput, IonButton, IonIcon, IonTitle, IonCardSubtitle, IonSpinner } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { eye, eyeOff, lockClosed, logInOutline, planetOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonInput, IonButton, IonIcon, IonTitle, IonSpinner, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class LoginPage implements OnInit {
+  private formBuilder = inject(NonNullableFormBuilder);
+  public showPassword: boolean = false;
+  public loading: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ lockClosed, eye, eyeOff, logInOutline, planetOutline })
   }
 
+  form: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {}
 }

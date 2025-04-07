b56c3341f1c78d6478379a2b81defec066e56a0b
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-04-07 17:44:09 -0300

    corrige status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { StatusBar } from '@capacitor/status-bar';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  ngOnInit(): void {
-    StatusBar.setOverlaysWebView({ overlay: true });
-  }
 }

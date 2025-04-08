599cd5631c9c1fa237a4f9ce9669fd7b84be1a14
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-04-08 10:32:50 -0300

    feat: ajusta status bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Tracker',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    "StatusBar": {
+      "overlaysWebView": false,
+      "style": "DARK",
+      "backgroundColor": "#ffffff99"
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { initializeApp } from '@angular/fire/app';
+import { Component, inject, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
+import { ToastService } from './services/toast/toast.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  standalone: true,
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private platform = inject(Platform);
+  private toast = inject(ToastService);
 
+  async initializeApp() {
+    await this.platform.ready();
+    this.toast.show('App Initialized', 'success');
+  }
+
+  async ngOnInit() {
+    await this.platform.ready();
+    await StatusBar.show();
+  }
 }
